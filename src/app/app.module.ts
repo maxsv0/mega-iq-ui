0af9d360f6cb1715e8cf1427a56807de39e545e0
maxsv0 <max.svistunov@gmail.com>
2018-11-18 08:56:00 +0100

    generate  files
    ng new mega-iq-ui
    ========
    
    ng g component index
    ng g component login
    ng g component register
    ng g component home
    ng g component alert
    
    ng g class _models/user
    
    ng g service _services/alert
    ng g service _services/authentication
    ng g service _services/user
    
    ng g guard _guards/auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { AlertComponent } from './alert/alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
