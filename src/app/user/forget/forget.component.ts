3c298fa1294736b528a26db493abf958bbb3edc5
maxsv0 <max.svistunov@gmail.com>
2018-12-02 20:14:29 +0100

    forget component

--- src/app/user/forget/forget.component.ts
+++ src/app/user/forget/forget.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
-import {AuthenticationService} from '@/_services';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { AlertService, UserService, AuthenticationService } from '@/_services';
 
 @Component({
   selector: 'app-forget',
@@ -8,10 +11,16 @@ import {AuthenticationService} from '@/_services';
   styleUrls: ['./forget.component.scss']
 })
 export class ForgetComponent implements OnInit {
+  forgetForm: FormGroup;
+  loading = false;
+  submitted = false;
 
   constructor(
+    private formBuilder: FormBuilder,
     private router: Router,
     private authenticationService: AuthenticationService,
+    private userService: UserService,
+    private alertService: AlertService
   ) {
     // redirect to home if already logged in
     if (this.authenticationService.currentUserValue) {
@@ -20,6 +29,35 @@ export class ForgetComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.forgetForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
   }
 
+  // convenience getter for easy access to form fields
+  get f() { return this.forgetForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.forgetForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+
+    this.loading = true;
+    this.userService.forget(this.forgetForm.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.alertService.success('Email containing new password was sent successful', true);
+          this.router.navigate(['/forget']);
+        },
+        error => {
+          this.alertService.error(error);
+          this.loading = false;
+        });
+  }
 }
