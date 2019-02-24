4dd7463a4bc13ce8436af5367b2900d879a9c022
maxsv0 <max.svistunov@gmail.com>
2019-02-24 19:34:37 +0100

    Add 2x images

--- src/app/_models/answer.ts
+++ src/app/_models/answer.ts
@@ -1,4 +1,5 @@
 export class Answer {
   id: number;
   pic: string;
+  pic2x: string;
 }
--- src/app/_models/question.ts
+++ src/app/_models/question.ts
@@ -2,6 +2,7 @@ import {Answer} from '@/_models';
 
 export class Question {
   pic: string;
+  pic2x: string;
   answerCorrect: number;
   answerUser: number;
   title: string;
