ee0f2a95fc85f90e86f8a4e00e87ba8e76b67224
maxsv0 <max.svistunov@gmail.com>
2019-05-09 14:05:34 +0200

    environment for dev can't be ignored

---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  apiUrl: 'http://de.mega-iq.com/api/v1',
+  apiGeoIpUrl: 'https://geoip.mega-iq.com',
+  firebase: {
+    apiKey: '',
+    authDomain: 'megaiq637.firebaseapp.com',
+    databaseURL: 'https://megaiq637.firebaseio.com',
+    projectId: 'megaiq637',
+    storageBucket: 'megaiq637.appspot.com',
+    messagingSenderId: '589199945075'
+  }
+};
