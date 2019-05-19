2610f2e73cee7f29cd26cb00ad42cd9baf5a2f32
Ainel <ainelainel@hotmail.com>
2019-05-19 21:03:55 +0200

    add action and file upload loaders - #14

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,47 +12,48 @@ import {TestTypeEnum} from '@/_models/enum';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mega-iq-ui';
-  currentUser: User;
-  backgroundClass: string;
-  testTypes: IqTest[] = [];
+	title = 'mega-iq-ui';
+	currentUser: User;
+	backgroundClass: string;
+	testTypes: IqTest[] = [];
+	loading: boolean = false;
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    private iqTestService: IqTestService,
-    private authenticationService: AuthenticationService
-  ) {
-    this.authenticationService.currentUser.subscribe(
-      user => {
-        this.currentUser = user;
-      }
-    );
+	constructor(
+		private route: ActivatedRoute,
+		private router: Router,
+		private iqTestService: IqTestService,
+		private authenticationService: AuthenticationService
+	) 	{
+		this.authenticationService.currentUser.subscribe(
+		user => {
+			this.currentUser = user;
+		}
+	);
 
-    this.testTypes = this.iqTestService.getIqTest();
+	this.testTypes = this.iqTestService.getIqTest();
 
-    router.events.subscribe((event) => {
-      if (event instanceof NavigationStart) {
-        if (event.url.startsWith('/iqtest/')) {
-          this.backgroundClass = event.url.substr(8);
-        } else if (event.url.startsWith('/classroom/')) {
-          this.backgroundClass = '';
-          // this.testTypes.forEach(
-          //   (testType) => {
-          //     if (test.type === testType.type) {
-          //       this.backgroundClass = testType.styleName;
-          //     }
-          //   }
-          // );
-        } else if (!event.url.startsWith('/classroom/')) {
-          this.backgroundClass = '';
-        }
-      }
-    });
-  }
+	router.events.subscribe((event) => {
+	if (event instanceof NavigationStart) {
+		if (event.url.startsWith('/iqtest/')) {
+			this.backgroundClass = event.url.substr(8);
+		} else if (event.url.startsWith('/classroom/')) {
+			this.backgroundClass = '';
+		// this.testTypes.forEach(
+		//   (testType) => {
+		//     if (test.type === testType.type) {
+		//       this.backgroundClass = testType.styleName;
+		//     }
+		//   }
+		// );
+		} else if (!event.url.startsWith('/classroom/')) {
+			this.backgroundClass = '';
+		}
+	}
+	});
+}
 
-  logout() {
-    this.authenticationService.logout();
-    this.router.navigate(['/login']);
-  }
+	logout() {
+		this.authenticationService.logout();
+		this.router.navigate(['/login']);
+	}
 }
