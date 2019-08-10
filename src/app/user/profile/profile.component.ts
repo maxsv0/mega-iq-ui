945b9f3be220c344a4bde38a778f543fe6d05f5b
Ainel <ainelainel@hotmail.com>
2019-08-10 10:25:41 +0200

    minor changes on home page table

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -10,44 +10,44 @@ templateUrl: './profile.component.html',
 styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-currentUser: User;
+    currentUser: User;
 
-@Input()
-user: User;
+    @Input()
+    user: User;
 
-constructor(
-    private route: ActivatedRoute,
-    private authenticationService: AuthenticationService,
-    private userService: UserService,
-    private alertService: AlertService
-) {
-    if (this.authenticationService.currentUserValue) {
-    this.currentUser = this.authenticationService.currentUserValue;
-    }
-}
-
-ngOnInit() {
-    if (this.user != null) {
-    return;
-    }
-
-    const userId = this.route.snapshot.params['userId'];
-    if (userId == null) {
-    return;
-    }
-
-    this.userService.getById(userId)
-    .pipe(first())
-    .subscribe(
-        apiResponseUser => {
-        if (apiResponseUser.ok) {
-            this.user = apiResponseUser.user;
-        } else {
-            this.alertService.error(apiResponseUser.msg);
+    constructor(
+        private route: ActivatedRoute,
+        private authenticationService: AuthenticationService,
+        private userService: UserService,
+        private alertService: AlertService
+    ) {
+        if (this.authenticationService.currentUserValue) {
+        this.currentUser = this.authenticationService.currentUserValue;
         }
-        },
-        error => {
-        this.alertService.error('API Service Unavailable. ' + error);
-        });
-}
+    }
+
+    ngOnInit() {
+        if (this.user != null) {
+            return;
+        }
+
+        const userId = this.route.snapshot.params['userId'];
+            if (userId == null) {
+            return;
+        }
+
+        this.userService.getById(userId)
+        .pipe(first())
+        .subscribe(
+            apiResponseUser => {
+                if (apiResponseUser.ok) {
+                    this.user = apiResponseUser.user;
+                } else {
+                    this.alertService.error(apiResponseUser.msg);
+                }
+            },
+            error => {
+                this.alertService.error('API Service Unavailable. ' + error);
+            });
+    }
 }
