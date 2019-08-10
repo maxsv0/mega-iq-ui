7126c60502a48d6cf6c10dcf491f2ad4a27d55f2
Ainel <ainelainel@hotmail.com>
2019-08-10 18:16:34 +0200

    add carousel to example profiles - #3

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -103,4 +103,31 @@ export class IndexComponent implements OnInit, OnDestroy {
       }
     );
   }
+
+    carouselOptions = {
+        margin: 25,
+        nav: true,
+        navText: ["<div class='nav-btn prev-slide'></div>", "<div class='nav-btn next-slide'></div>"],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 1,
+                nav: true
+            },
+            1000: {
+                items: 2,
+                nav: true,
+                loop: false
+            },
+            1500: {
+                items: 3,
+                nav: true,
+                loop: false
+            }
+        }
+    }
 }
