196d9cf1834ab439057cf150e9bba0f92e56776d
maxsv0 <max.svistunov@gmail.com>
2019-08-21 20:08:38 +0200

    Revert "Enable SSR" (some)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ru.mega-iq.com/api/v1',
+  apiUrl: 'https://www.mega-iq.com/api/v1',
   apiGeoIpUrl: 'https://geoip.mega-iq.com',
   firebase: {
-    apiKey: 'AIzaSyA2DRW5ImkUR8C7wg27B8ooCexjDZcCrqw',
+    apiKey: '',
     authDomain: 'megaiq637.firebaseapp.com',
     databaseURL: 'https://megaiq637.firebaseio.com',
     projectId: 'megaiq637',
