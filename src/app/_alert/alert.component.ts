c3fe6e5f23a9182fff902b7d3fabcee6611e4a1d
Ainel <ainelainel@hotmail.com>
2019-09-28 21:40:57 +0200

    add alert styles and timeout - #80

--- src/app/_alert/alert.component.ts
+++ src/app/_alert/alert.component.ts
@@ -18,7 +18,13 @@ export class AlertComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.subscription = this.alertService.getMessage().subscribe(message => {
-      this.message = message;
+        this.message = message;
+        if(this.message) {
+            const self = this;
+            setTimeout(() => {
+                self.message = null;
+            }, 5000);
+        }
     });
   }
 
