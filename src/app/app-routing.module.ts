eecbb7850ac1391e5854d7db0c99ded32d631a9f
maxsv0 <max.svistunov@gmail.com>
2020-07-27 22:55:32 +0200

    npm update all

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -37,4 +37,6 @@ const appRoutes: Routes = [
   {path: '**', redirectTo: ''}
 ];
 
-export const routing = RouterModule.forRoot(appRoutes);
+export const routing = RouterModule.forRoot(appRoutes, {
+    initialNavigation: 'enabled'
+});
